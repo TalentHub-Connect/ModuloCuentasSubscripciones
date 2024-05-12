9fa47adf0aee2de946d1237686ac91fcefe8f506
ticaso <ticaso999@gmail.com>
2024-05-12 09:43:53 -0500

    initial configuration

---
+++ src/main/java/com/cuentas/subscription/model/Subscription.java
@@ -0,0 +1,147 @@
+package com.cuentas.subscription.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "namecompany")
+    private String nameCompany;
+
+    @Column(name = "nit")
+    private int nit;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phonecompany")
+    private int phoneCompany;
+
+    @Column(name = "namelegalrepresentative")
+    private String nameLegalRepresentative;
+
+    @Column(name = "idlegalrepresentative")
+    private int idLegalRepresentative;
+
+    @Column(name = "linkdate")
+    private String linkDate;
+
+    @Column(name = "subscriptionenddate")
+    private String subscriptionEndDate;
+
+    @Column(name = "numworkers")
+    private int numWorkers;
+
+    @Column(name = "status")
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneCompany() {
+        return phoneCompany;
+    }
+
+    public void setPhoneCompany(int phoneCompany) {
+        this.phoneCompany = phoneCompany;
+    }
+
+    public String getNameLegalRepresentative() {
+        return nameLegalRepresentative;
+    }
+
+    public void setNameLegalRepresentative(String nameLegalRepresentative) {
+        this.nameLegalRepresentative = nameLegalRepresentative;
+    }
+
+    public int getIdLegalRepresentative() {
+        return idLegalRepresentative;
+    }
+
+    public void setIdLegalRepresentative(int idLegalRepresentative) {
+        this.idLegalRepresentative = idLegalRepresentative;
+    }
+
+    public String getLinkDate() {
+        return linkDate;
+    }
+
+    public void setLinkDate(String linkDate) {
+        this.linkDate = linkDate;
+    }
+
+    public String getSubscriptionEndDate() {
+        return subscriptionEndDate;
+    }
+
+    public void setSubscriptionEndDate(String subscriptionEndDate) {
+        this.subscriptionEndDate = subscriptionEndDate;
+    }
+
+    public int getNumWorkers() {
+        return numWorkers;
+    }
+
+    public void setNumWorkers(int numWorkers) {
+        this.numWorkers = numWorkers;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
